ca279c45c8dfd854552b7e089da17d363c65276c
HenrySmith3 <henry.smith@gatech.edu>
2013-07-30 20:10:25 -0400

    Adding dropbox things

--- src/com/example/AndroidJournal/MyActivity.java
+++ src/com/example/AndroidJournal/MyActivity.java
@@ -2,8 +2,14 @@ package com.example.AndroidJournal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.dropbox.client2.session.Session;
 
 public class MyActivity extends Activity {
+
+    final static private String APP_KEY = "INSERT_APP_KEY";
+    final static private String APP_SECRET = "INSERT_APP_SECRET";
+    final static private Session.AccessType ACCESS_TYPE = Session.AccessType.DROPBOX;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +17,8 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView textBox = (TextView) findViewById(R.id.textView);
+
+        textBox.setText("Testing");
     }
 }
